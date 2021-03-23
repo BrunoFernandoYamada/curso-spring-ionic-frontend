da17595f78985bcf3008b4f3dabdf503f4bc1867
Bruno Yamada <yamada_bruno@hotmail.com>
2021-03-22 21:26:29 -0300

    Salvando novo cliente

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CidadeDTO } from '../../models/cidade.dto';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeService } from '../../services/domain/cidade.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { EstadoService } from '../../services/domain/estado.service';
 
 
@@ -22,13 +23,15 @@ export class SignupPage {
     public navParams: NavParams, 
     public formBuilder: FormBuilder,
     public cidadeService: CidadeService,
-    public estadoService: EstadoService) {
+    public estadoService: EstadoService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController) {
 
       this.formGroup = this.formBuilder.group({
         nome: ['Joaquin', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
         email: ['joaquin@gmail.com',[Validators.required, Validators.email]],
         tipo: ['1', [Validators.required]],
-        cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.max(14)]],
+        cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
         senha: ['123', [Validators.required]],
         logradouro: ['Rua Via', [Validators.required]],
         numero: ['25', [Validators.required]],
@@ -65,7 +68,28 @@ export class SignupPage {
   }
 
   signupUser(){
-    console.log("Passou");
+    this.clienteService.insert(this.formGroup.value)
+      .subscribe(response => {
+        this.showInsertOk();
+      },
+      errors => {});
+  }
+
+  showInsertOk(){
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons:[
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 
