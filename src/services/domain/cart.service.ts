fe39fa006b1d19eb9d99f2eb3210b8448ffb47eb
Bruno Yamada <yamada_bruno@hotmail.com>
2021-03-24 23:39:07 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -34,4 +34,51 @@ export class CartService{
         return cart;
     }
 
+    removeProduto(produto : ProdutoDTO) : Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items.splice(position, 1)
+        }
+        this.storageServe.setCart(cart);
+        return cart;
+    }
+
+    increaseQuantity(produto : ProdutoDTO) : Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade++;
+        }
+        this.storageServe.setCart(cart);
+        return cart;
+    }
+
+    decreaseQuantity(produto : ProdutoDTO) : Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade--;
+            if(cart.items[position].quantidade < 1) {
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storageServe.setCart(cart);
+        return cart;
+    }
+
+    total() : number{
+        let cart = this.getCart();
+        let sum = 0;
+        for(var i = 0; i <cart.items.length; i++){
+            sum += cart.items[i].produto.preco * cart.items[i].quantidade;
+        }
+        return sum;
+    }
+
+
+    
+
+
+
 }
