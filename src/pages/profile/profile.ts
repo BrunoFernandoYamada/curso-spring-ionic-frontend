c1180fcb7b83cc21e7941537e81034c9af57d8c8
Bruno Yamada <yamada_bruno@hotmail.com>
2021-05-04 20:58:47 -0300

    fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,6 +34,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocaUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -79,6 +83,21 @@ export class ProfilePage {
     });
     
 
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    }, 
+    error => {
+
+    });
+  }
+
+  cancel(){
+    this.picture  = null;
   }
 
 
