18406b6cf1430258bcfffe2e25cb53ca5625aad9
Bruno Yamada <yamada_bruno@hotmail.com>
2021-05-05 22:29:19 -0300

    Testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://byamada-curso-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://bfyamada-curso-spring.s3-sa-east-1.amazonaws.com"
 }
